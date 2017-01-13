5bc084c682dbf2968362dbcb5f8aa53dbfbac58f
Ganesh Sittampalam <ganesh@earth.li>
2017-01-13 06:10:15 +0000

    Restore htmlunit-driver support to selenium-server-standalone It was moved to a separate project between versions 2.45 and 2.53: https://github.com/SeleniumHQ/selenium/commit/2d3150b

---
+++ pkgs/development/tools/selenium/htmlunit-driver/default.nix
@@ -0,0 +1,29 @@
+{ stdenv, fetchurl }:
+
+with stdenv.lib;
+
+stdenv.mkDerivation rec {
+  name = "htmlunit-driver-standalone-${version}";
+  version = "2.21";
+
+  src = fetchurl {
+    url = "https://github.com/SeleniumHQ/htmlunit-driver/releases/download/${version}/htmlunit-driver-standalone-${version}.jar";
+    sha256 = "1wrbam0hb036717z3y73lsw4pwp5sdiw2i1818kg9pvc7i3fb3yn";
+  };
+
+  unpackPhase = "true";
+
+  buildInputs = [ ];
+
+  installPhase = ''
+    mkdir -p $out/share/lib/${name}
+    cp $src $out/share/lib/${name}/${name}.jar
+  '';
+
+  meta = {
+    homepage = https://github.com/SeleniumHQ/htmlunit-driver;
+    description = "A WebDriver server for running Selenium tests on the HtmlUnit headless browser";
+    platforms = platforms.all;
+    license = licenses.asl20;
+  };
+}
